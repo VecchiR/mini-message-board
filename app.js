44588b23a6d7134362e9caa2dff2750e2879f7c4
Vecchi <79600745+VecchiR@users.noreply.github.com>
2025-01-24 15:53:53 -0300

    Display pre-existing messages on index pg

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
+
 
 const app = express();
 
 const PORT = 3000;
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.listen(PORT, () => {
     console.log('server running on port', PORT);
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,8 +2,21 @@ const { Router } = require("express");
 
 const router = Router();
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date()
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date()
+    }
+];
+
 router.get('/', (req, res) => {
-    res.send("index page");
+    res.render("index", { title: "Mini Messageboard", messages: messages })
 });
 
 module.exports = router;
